75c1616393a7028de30602c12513480ac46734d3
TheJP <jp_55555@hotmail.com>
2025-12-04 06:20:40 +0100

    Solved day 4 part 1
    Trying out complex number indices for the first time. Thanks to:
    https://will-keleher.com/posts/2d-navigation-with-imaginary-numbers-is-better.html

---
+++ day04/one.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+import sys
+
+input = open(sys.argv[1]).read().splitlines()
+
+grid = defaultdict(lambda: '#')
+for r, row in enumerate(input):
+    for c, char in enumerate(row):
+        grid[r * 1j + c] = char
+
+h = len(input)
+w = len(input[0])
+
+total = 0
+for y in range(h):
+    for x in range(w):
+        current = y * 1j + x
+        if grid[current] != "@":
+            continue
+
+        ns = 0
+        for adj in (1, -1, 1j, -1j, 1 + 1j, 1 - 1j, -1 + 1j, -1 - 1j):
+            if grid[current + adj] == "@":
+                ns += 1
+
+        if ns < 4:
+            total += 1
+
+print(total)
